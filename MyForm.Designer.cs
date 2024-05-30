7e86b68aad964afce31a51ce37d71ae8c01b4aa5
Nazar832 <nazargrubij8@gmail.com>
2024-05-30 11:53:25 +0300

    Initial commit

---
+++ MyForm.Designer.cs
@@ -0,0 +1,214 @@
+﻿
+namespace ParallelepipedMethod
+{
+    partial class MyForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panel = new System.Windows.Forms.Panel();
+            this.doResultsMatchLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.parallelTimeTextBox = new System.Windows.Forms.TextBox();
+            this.sequentialTimeTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.nextLoopButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            this.panel.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chart
+            // 
+            this.chart.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.AxisX.Title = "Ітерація";
+            chartArea1.AxisX.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            chartArea1.AxisY.Title = "X(i, j)";
+            chartArea1.AxisY.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            chartArea1.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart.Legends.Add(legend1);
+            this.chart.Location = new System.Drawing.Point(0, 0);
+            this.chart.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.chart.Name = "chart";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "Послідовно";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.IsVisibleInLegend = false;
+            series2.Legend = "Legend1";
+            series2.Name = "Паралельно";
+            this.chart.Series.Add(series1);
+            this.chart.Series.Add(series2);
+            this.chart.Size = new System.Drawing.Size(843, 502);
+            this.chart.TabIndex = 0;
+            this.chart.Text = "chart";
+            title1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            title1.Name = "Title1";
+            this.chart.Titles.Add(title1);
+            // 
+            // panel
+            // 
+            this.panel.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.panel.Controls.Add(this.doResultsMatchLabel);
+            this.panel.Controls.Add(this.label3);
+            this.panel.Controls.Add(this.parallelTimeTextBox);
+            this.panel.Controls.Add(this.sequentialTimeTextBox);
+            this.panel.Controls.Add(this.label2);
+            this.panel.Controls.Add(this.label1);
+            this.panel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel.Location = new System.Drawing.Point(843, 0);
+            this.panel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(268, 586);
+            this.panel.TabIndex = 13;
+            // 
+            // doResultsMatchLabel
+            // 
+            this.doResultsMatchLabel.AutoSize = true;
+            this.doResultsMatchLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.doResultsMatchLabel.Location = new System.Drawing.Point(32, 267);
+            this.doResultsMatchLabel.Name = "doResultsMatchLabel";
+            this.doResultsMatchLabel.Size = new System.Drawing.Size(0, 18);
+            this.doResultsMatchLabel.TabIndex = 18;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label3.Location = new System.Drawing.Point(65, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(139, 18);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Час в мілісекундах";
+            // 
+            // parallelTimeTextBox
+            // 
+            this.parallelTimeTextBox.Location = new System.Drawing.Point(140, 112);
+            this.parallelTimeTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.parallelTimeTextBox.Name = "parallelTimeTextBox";
+            this.parallelTimeTextBox.Size = new System.Drawing.Size(100, 22);
+            this.parallelTimeTextBox.TabIndex = 16;
+            // 
+            // sequentialTimeTextBox
+            // 
+            this.sequentialTimeTextBox.Location = new System.Drawing.Point(140, 68);
+            this.sequentialTimeTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.sequentialTimeTextBox.Name = "sequentialTimeTextBox";
+            this.sequentialTimeTextBox.Size = new System.Drawing.Size(100, 22);
+            this.sequentialTimeTextBox.TabIndex = 15;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label2.Location = new System.Drawing.Point(28, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 18);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Паралельно:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label1.Location = new System.Drawing.Point(28, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 18);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Послідовно:";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Cornsilk;
+            this.panel1.Controls.Add(this.nextLoopButton);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 508);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(843, 78);
+            this.panel1.TabIndex = 14;
+            // 
+            // nextLoopButton
+            // 
+            this.nextLoopButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.nextLoopButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.nextLoopButton.Location = new System.Drawing.Point(345, 15);
+            this.nextLoopButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.nextLoopButton.Name = "nextLoopButton";
+            this.nextLoopButton.Size = new System.Drawing.Size(152, 46);
+            this.nextLoopButton.TabIndex = 11;
+            this.nextLoopButton.Text = "Наступний цикл";
+            this.nextLoopButton.UseVisualStyleBackColor = true;
+            this.nextLoopButton.Click += new System.EventHandler(this.nextLoopButton_Click);
+            // 
+            // MyForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1111, 586);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.chart);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "MyForm";
+            this.Text = "Форма";
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            this.panel.ResumeLayout(false);
+            this.panel.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox parallelTimeTextBox;
+        private System.Windows.Forms.TextBox sequentialTimeTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button nextLoopButton;
+        private System.Windows.Forms.Label doResultsMatchLabel;
+    }
+}
+
---
+++ MyForm.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ParallelepipedMethod
+{
+    public partial class MyForm : Form
+    {
+        private int p1, p2;
+
+        private int iInitial, iFinal;
+        private int jInitial, jFinal;
+
+        private int maxGraphSize = 5000;
+
+        private bool currentLoopIsParallel = false;
+
+        private int numberOfCurrentLoop = 1;
+        private int loopsNumber = 3;
+
+        private double[,] X_sequential;
+        private double[,] X_parallel;
+
+        private int rowsNumber;
+        private int colsNumber;
+
+        // події, які сигналізують, що відповідні ітерації виконані
+        private static ManualResetEventSlim[,] readyEvents;
+
+        public MyForm()
+        {
+            InitializeComponent();
+
+            nextLoopButton_Click(null, EventArgs.Empty);
+        }
+        
+        private void initializeMatrixes()
+        {
+            rowsNumber = iFinal - iInitial + 1;
+            colsNumber = jFinal - jInitial + 1;
+
+            X_sequential = new double[rowsNumber, colsNumber];
+            X_parallel = new double[rowsNumber, colsNumber];
+
+            Random rnd = new Random();
+            for (int i = 0; i < rowsNumber; i++)
+            {
+                for (int j = 0; j < colsNumber; j++)
+                {
+                    double value = Math.Pow(i, 3) * Math.Sqrt(i * j); // задання 1-го набору вхідних даних
+                    //double value = 0; // задання 2-го набору вхідних даних
+                    //double value = i / (j + 1); // задання 3-го набору вхідних даних
+
+                    X_sequential[i, j] = value;
+                    X_parallel[i, j] = value;
+                }
+            }
+        }
+
+        // функція перетворення двовимірного масиву X в одновимірний "стрічковий" вигляд для того, щоб намалювати графік
+        private double[] makeArrayFromMatrix(double[,] matrix)
+        {
+            int colsNumber = matrix.GetLength(1);
+            int rowsNumber = matrix.Length > maxGraphSize ? Math.Max(maxGraphSize / colsNumber, 1) : matrix.GetLength(0);
+
+            double[] arr = new double[Math.Min(maxGraphSize, matrix.Length)];
+            for (int i = 0; i < rowsNumber; i++)
+            {
+                for (int j = 0; j < colsNumber; j++)
+                {
+                    int index = i * colsNumber + j;
+                    if (index == maxGraphSize) return arr;
+                    arr[index] = matrix[i, j];
+                }
+            }
+
+            return arr;
+        }
+
+        private bool resultsMatch()
+        {
+            for (int i = 0; i < rowsNumber; i++)
+            {
+                for (int j = 0; j < colsNumber; j++)
+                {
+                    if (X_sequential[i, j] != X_parallel[i, j]) return false;                                       
+                }
+            }
+
+            return true;
+        }
+
+        // послідовний цикл
+        private void sequentialLoop()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            switch (numberOfCurrentLoop)
+            {
+                case 1:
+                    for (int i = iInitial; i <= iFinal; i++)
+                    {
+                        for (int j = jInitial; j <= jFinal; j++)
+                        {
+                            int row = i - iInitial;
+                            int col = j - jInitial;
+
+                            bool dependsOnRowMinus2ColMinus1 = row - 2 >= 0 && col - 1 >= 0;
+                            bool dependsOnRowMinus2ColPlus1 = row - 2 >= 0 && col + 1 < colsNumber;
+
+                            double value1 = dependsOnRowMinus2ColMinus1 ? X_sequential[row - 2, col - 1] : X_sequential[row, col];
+                            double value2 = dependsOnRowMinus2ColPlus1 ? X_sequential[row - 2, col + 1] : X_sequential[row, col];
+
+                            for (int y = 0; y < 100; y++) X_sequential[row, col] = Math.Pow(Math.Sin(value1), 3) + Math.Sqrt(value2) + 10;
+                        }
+                    }
+                    break;
+                case 2:
+                    for (int i = iInitial; i <= iFinal; i++)
+                    {
+                        for (int j = jInitial; j <= jFinal; j++)
+                        {
+                            int row = i - iInitial;
+                            int col = j - jInitial;
+                            bool iterationIsDependent = row - 2 >= 0 && col - 1 >= 0;
+                            double value = iterationIsDependent ? X_sequential[row - 2, col - 1] : X_sequential[row, col];
+                            for (int y = 0; y < 100; y++) X_sequential[row, col] = Math.Log(value + 2) / (20 * Math.Exp(value));
+                        }
+                    }
+                    break;
+                case 3:
+                    for (int i = iInitial; i <= iFinal; i++)
+                    {
+                        for (int j = jInitial; j <= jFinal; j++)
+                        {
+                            int row = i - iInitial;
+                            int col = j - jInitial;
+                            bool iterationIsDependent = row - 3 >= 0 && 2 * col - 3 >= 0 && 2 * col - 3 < colsNumber;
+                            double value = iterationIsDependent ? X_sequential[row - 3, 2 * col - 3] : X_sequential[row, col];
+                            for (int y = 0; y < 100; y++) X_sequential[row, col] = Math.Log(Math.Cos(2 * Math.Pow(value, 4)) + 10);
+                        }
+                    }
+                    break;
+            }
+
+            sw.Stop();
+            TimeSpan elapsedTime = sw.Elapsed;
+            sequentialTimeTextBox.Text = Math.Round(elapsedTime.TotalMilliseconds).ToString();
+        }
+
+        // паралельний цикл
+        private void parallelLoop()
+        {           
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            switch (numberOfCurrentLoop) 
+            {
+                case 1:
+                    for (int k = 1; k <= p1; k++)
+                    {
+                        int localK = k;
+                        ThreadPool.QueueUserWorkItem(state1 =>
+                        {
+                            for (int l = 1; l <= p2; l++)
+                            {
+                                int localL = l;
+                                ThreadPool.QueueUserWorkItem(state2 =>
+                                {
+                                    for (int i = localK + iInitial - 1; i <= iFinal; i += p1)
+                                    {
+                                        for (int j = localL + jInitial - 1; j <= jFinal; j += p2)
+                                        {
+                                            int row = i - iInitial;
+                                            int col = j - jInitial;
+
+                                            bool dependsOnRowMinus2ColMinus1 = row - 2 >= 0 && col - 1 >= 0;
+                                            bool dependsOnRowMinus2ColPlus1 = row - 2 >= 0 && col + 1 < colsNumber;
+
+                                            if (dependsOnRowMinus2ColMinus1) readyEvents[row - 2, col - 1].Wait();
+                                            if (dependsOnRowMinus2ColPlus1) readyEvents[row - 2, col + 1].Wait();
+
+                                            double value1 = dependsOnRowMinus2ColMinus1 ? X_parallel[row - 2, col - 1] : X_parallel[row, col];
+                                            double value2 = dependsOnRowMinus2ColPlus1 ? X_parallel[row - 2, col + 1] : X_parallel[row, col];
+
+                                            for (int y = 0; y < 100; y++) X_parallel[row, col] = Math.Pow(Math.Sin(value1), 3) + Math.Sqrt(value2) + 10;
+                                            readyEvents[row, col].Set();
+                                        }
+                                    }
+                                });
+                            }
+                        });
+                    }
+                    break;
+                case 2:
+                    for (int k = 1; k <= p1; k++)
+                    {
+                        int localK = k;
+                        ThreadPool.QueueUserWorkItem(state =>
+                        {
+                            for (int l = 1; l <= p2; l++)
+                            {
+                                for (int i = localK + iInitial - 1; i <= iFinal; i += p1)
+                                {
+                                    for (int j = l + jInitial - 1; j <= jFinal; j += p2)
+                                    {
+                                        int row = i - iInitial;
+                                        int col = j - jInitial;
+                                        bool iterationIsDependent = row - 2 >= 0 && col - 1 >= 0;
+                                        if (iterationIsDependent) readyEvents[row - 2, col - 1].Wait();
+                                        double value = iterationIsDependent ? X_parallel[row - 2, col - 1] : X_parallel[row, col];
+                                        for (int y = 0; y < 100; y++) X_parallel[row, col] = Math.Log(value + 2) / (20 * Math.Exp(value));
+                                        readyEvents[row, col].Set();
+                                    }
+                                }
+                            }
+                        });
+                    }
+                    break;
+                case 3:
+                    for (int k = 1; k <= p1; k++)
+                    {
+                        int localK = k;
+                        ThreadPool.QueueUserWorkItem(state1 =>
+                        {
+                            for (int l = 1; l <= p2; l++)
+                            {
+                                int localL = l;
+                                ThreadPool.QueueUserWorkItem(state2 => {
+                                    for (int i = localK + iInitial - 1; i <= iFinal; i += p1)
+                                    {
+                                        for (int j = localL + jInitial - 1; j <= jFinal; j += p2)
+                                        {
+                                            int row = i - iInitial;
+                                            int col = j - jInitial;
+                                            bool iterationIsDependent = row - 3 >= 0 && 2 * col - 3 >= 0 && 2 * col - 3 < colsNumber;
+                                            if (iterationIsDependent) readyEvents[row - 3, 2 * col - 3].Wait();
+                                            double value = iterationIsDependent ? X_parallel[row - 3, 2 * col - 3] : X_parallel[row, col];
+                                            for (int y = 0; y < 100; y++) X_parallel[row, col] = Math.Log(Math.Cos(2 * Math.Pow(value, 4)) + 10);
+                                            readyEvents[row, col].Set();
+                                        }
+                                    }
+                                });                               
+                            }
+                        });
+                    }
+                    break;
+            }
+
+            // чекаємо повного завершення циклу
+            for (int i = 0; i < rowsNumber; i++)
+            {
+                for (int j = 0; j < colsNumber; j++)
+                {
+                    readyEvents[i, j].Wait();
+                }
+            }
+
+            sw.Stop();
+            TimeSpan elapsedTime = sw.Elapsed;
+            parallelTimeTextBox.Text = Math.Round(elapsedTime.TotalMilliseconds).ToString();
+        }
+
+        private void nextLoopButton_Click(object sender, EventArgs e)
+        {
+            nextLoopButton.Enabled = false;
+
+            if (!currentLoopIsParallel) {
+                chart.Series[0].Points.Clear();
+                chart.Series[1].Points.Clear();
+                chart.Series[1].IsVisibleInLegend = false;
+
+                doResultsMatchLabel.Text = "";
+
+                sequentialTimeTextBox.Clear();
+                parallelTimeTextBox.Clear();
+
+                chart.Titles[0].Text = "Результати виконання " + numberOfCurrentLoop + "-ї пари циклів";
+
+                // встановлення параметрів для поточного циклу
+                switch (numberOfCurrentLoop)
+                {
+                    case 1:
+                        p1 = 2; p2 = 6;
+                        iInitial = 1; iFinal = 100000;
+                        jInitial = 1; jFinal = 6;
+                        break;
+                    case 2:
+                        p1 = 60000; p2 = 1;
+                        iInitial = 1; iFinal = 60000;
+                        jInitial = 1; jFinal = 24;
+                        break;
+                    case 3:
+                        p1 = 3; p2 = 9;
+                        iInitial = 1; iFinal = 75000;
+                        jInitial = 1; jFinal = 9;
+                        break;
+                }
+
+                initializeMatrixes();
+
+                sequentialLoop();
+
+                chart.Series[0].Points.DataBindY(makeArrayFromMatrix(X_sequential));
+
+                currentLoopIsParallel = true;
+            }
+            else
+            {
+                chart.Series[1].Points.Clear();
+                chart.Series[1].IsVisibleInLegend = true;
+
+                readyEvents = new ManualResetEventSlim[rowsNumber, colsNumber];
+                for (int i = 0; i < rowsNumber; i++)
+                {
+                    for (int j = 0; j < colsNumber; j++)
+                    {
+                        readyEvents[i, j] = new ManualResetEventSlim(false);
+                    }
+                }
+
+                parallelLoop();
+
+                currentLoopIsParallel = false;
+                numberOfCurrentLoop = numberOfCurrentLoop % loopsNumber + 1;
+
+                chart.Series[1].Points.DataBindY(makeArrayFromMatrix(X_parallel));
+
+                // порівняння результатів послідовного та паралельного виконання
+                doResultsMatchLabel.Text = resultsMatch() ? "Результати співпадають!" : "Результати не співпадають!";
+            }
+
+            nextLoopButton.Enabled = true;
+        }      
+    }
+}
